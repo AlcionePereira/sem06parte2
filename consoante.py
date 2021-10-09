3fb822d4a7e97b450bbeef3984f6b6245f8fcfcc
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-10-09 20:44:49 -0300

    Add files via upload

---
+++ consoante.py
@@ -0,0 +1,10 @@
+caractere = input().lower().strip()
+if caractere in ('''   'b', 'c', 'd', 'f', 'g', 'h',
+'j', 'k','l', 'm', 'n', 'p', 'q', 'r', 's',
+'t', 'v', 'x', 'z'  '''):
+    print('True')
+
+else:
+    print('False')
+
+    
