54b4f8da74614cad47add7020ff588a6e067c9c5
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-10-09 21:01:09 -0300

    Add files via upload

---
+++ condi.py
@@ -0,0 +1,37 @@
+ca = input().lower()
+
+if ca in ('a', 'e', 'i', 'o', 'u'):
+    print('vogal')
+    
+    
+
+elif ca in ('b', 'c', 'd','f','g', 'h','j', 'k','l', 'm', 'n', 'p','q', 'r', 's','t', 'v','w','x', 'y','z'):
+    print('consoante')
+    
+    
+
+elif ca in ('0', '1', '2', '3', '4', '5', '6','7','8','9'):
+    print('número')
+    
+    
+
+else:
+    print('símbolo')
+    
+
+
+
+
+
+
+
+
+
+    
+    
+    
+         
+
+
+
+
