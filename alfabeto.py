3ee9ddca8998b77c13b9b0ee1aa1e50facb67e8c
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-10-09 21:11:30 -0300

    Add files via upload

---
+++ alfabeto.py
@@ -0,0 +1,9 @@
+caractere = input().lower().strip()
+
+if caractere in ('''  'a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i', 'j',
+'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+'v', 'w','x','y', 'z' '''):
+    print('True')
+
+else:
+    print('False')
